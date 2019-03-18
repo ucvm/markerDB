6d074fa9c210a10589e59f77521f24d8c0a90868
mworkentine <matt.workentine@gmail.com>
2019-03-18 14:03:58 -0600

    fixed write functions to properly write database when the taxonomy is not fixed

--- scripts/write_functions.R
+++ scripts/write_functions.R
@@ -5,18 +5,35 @@
 # --- dada2
 
 
+get_ranks = function(db) {
+	all_ranks = c("superkingdom", "kingdom", "phylum", "class", 
+								"order", "family", "genus", "species")
+		
+	ranks = colnames(db)[colnames(db) %in% all_ranks]
+	return(ranks)
+}
+
+clean_db = function(db) {
+	db = dplyr::mutate_all(db, ~stringr::str_replace_na(.x)) 
+	if ("species" %in% colnames(db)) {
+		db = dplyr::mutate(db, species = stringr::str_replace_all(species, " ", "_"))
+	}	
+	return(db)
+}
+
+
 write_functions = list(
 	dada2 = function(db, seqs, outdir, align = NULL) {
 		
 		outfasta = file.path(outdir,"dada2.fasta")
+		ranks = get_ranks(db)
 		
-		dada2 = db %>%
-			dplyr::mutate_all(~stringr::str_replace_na(.x)) %>%
-			dplyr::mutate(species = stringr::str_replace_all(species, " ", "_")) %>%
-			dplyr::mutate(tax = stringr::str_c(superkingdom, kingdom, phylum, class, order,
-																				 family, genus, species, sep = ";")) %>%
+		dada2 = db %>% 
+			clean_db() %>% 
+			dplyr::select(ranks) %>% 
+			tidyr::unite("tax", sep = ";") %>% 
 			dplyr::pull(tax)
-		
+	
 		Biostrings::writeXStringSet(setNames(seqs, dada2), outfasta)
 		
 		return(outfasta)
@@ -25,13 +42,13 @@ write_functions = list(
 	rdp = function(db, seqs, outdir, align = NULL) {
 		
 		outfasta = file.path(outdir, "rdp.fasta")
+		ranks = get_ranks(db)
 		
-		rdp = db %>%
-			dplyr::mutate_all(~stringr::str_replace_na(.x)) %>%
-			dplyr::mutate(species = stringr::str_replace_all(species, " ", "_")) %>%
-			dplyr::mutate(tax = stringr::str_c("Root", superkingdom, kingdom, phylum, class, order,
-																				 family, genus, species, sep = ";")) %>%
-			dplyr::select(accn, tax) %>%
+		rdp = db %>% 
+			clean_db() %>% 
+			mutate(root = "Root") %>% 
+			dplyr::select(accn, root, ranks) %>% 
+			tidyr::unite("tax", -accn, sep = ";") %>% 
 			dplyr::mutate(tax = stringr::str_c(accn, tax, sep = " ")) %>%
 			dplyr::pull(tax)
 		
@@ -47,10 +64,9 @@ write_functions = list(
 		outaln = file.path(outdir, "mothur.aln")
 		
 		mothur = db %>%
-			dplyr::mutate_all(~stringr::str_replace_na(.x)) %>%
-			dplyr::mutate(species = stringr::str_replace_all(species, " ", "_")) %>%
-			dplyr::mutate(tax = stringr::str_c(superkingdom, kingdom, phylum, class, order,
-																				 family, genus, species, sep = ";")) %>%
+			clean_db() %>% 
+			dplyr::select(accn, ranks) %>% 
+			tidyr::unite("tax", -accn, sep = ";") %>% 
 			dplyr::mutate(tax = stringr::str_c(tax, ";")) %>%
 			dplyr::select(accn, tax)
 		
